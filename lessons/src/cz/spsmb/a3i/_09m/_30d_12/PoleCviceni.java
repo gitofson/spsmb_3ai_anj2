478e3117e92394ec404bd8d6872b02eec2db9271
stemberk <stemberk@spsmb.cz>
2021-09-30 12:11:30 +0200

    Cviceni

---
+++ lessons/src/cz/spsmb/a3i/_09m/_30d_12/PoleCviceni.java
@@ -0,0 +1,5 @@
+package cz.spsmb.a3i._09m._30d_12;
+
+public class PoleCviceni {
+    //Napište program, který metodou Eratosthenova síta vypíše všecha prvočísla od 1 do N, kdy N zadáme z klávesnice
+}
