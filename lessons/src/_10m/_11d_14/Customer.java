5d20d2eeb3f68018a9478c1df636e7d7feb8ab8d
Radek Pelikán <radek.pelikan@student.spsmb.cz>
2021-10-11 11:53:32 +0200

    fixed

--- lessons/src/_10m/_11d_14/Customer.java
+++ lessons/src/_10m/_11d_14/Customer.java
@@ -1,36 +1,23 @@
 package _10m._11d_14;
 
-<<<<<<< HEAD
 /**
  * Ukončení práce s objekty (finalizer - opak konstruktoru
  */
-=======
 // Ukončení práce s objekty (finalizer - opak konstruktoru)
->>>>>>> b840612adda681e1125e8bbb0a74896d77c39e1f
 public class Customer {
     public static int nCustomers = 0;   // Proměnná třídy
     public int spent;
 
-<<<<<<< HEAD
-    // Potřebná znalost dědění (klíčová slova super, protected)
-    protected void finalize() throws Throwable{
-        Customer.nCustomers--;
-        System.out.println("Konec zákazníka");
-=======
     //potřebná znalost dědění (klíčová slova super, protected)
     protected void finalize() throws Throwable{
         nCustomers--;
         System.out.println("Konec zakaznika");
->>>>>>> b840612adda681e1125e8bbb0a74896d77c39e1f
         super.finalize();
     }
 
     public Customer() {
-<<<<<<< HEAD
         Customer.nCustomers++;
-=======
         nCustomers++;
->>>>>>> b840612adda681e1125e8bbb0a74896d77c39e1f
         this.spent = 0;
     }
 
@@ -39,19 +26,11 @@ public class Customer {
     }
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.format("Počet zákazníků: %d%n", nCustomers);
-        Customer a = new Customer();
-        System.out.format("Počet zákazníků: %d%n", nCustomers);
-        Customer b = new Customer();
-        System.out.format("Počet zákazníků: %d%n", nCustomers);
-=======
         System.out.format("Počet zákazníků: %d%n", Customer.nCustomers);
         Customer a = new Customer();
         System.out.format("Počet zákazníků: %d%n", Customer.nCustomers);
         Customer b = new Customer();
         System.out.format("Počet zákazníků: %d%n", Customer.nCustomers);
->>>>>>> b840612adda681e1125e8bbb0a74896d77c39e1f
 
         System.out.format("A utratil: %d%n", a.spent);
         a.buy(100);
@@ -60,17 +39,9 @@ public class Customer {
         System.out.format("B utratil: %d%n", b.spent);
         System.out.format("utratili: %d%n", a.spent + b.spent);
         a = null;
-<<<<<<< HEAD
         // Nutné vynucení si zrušení objektu (instance třídy Customer)
         System.runFinalization();   // Vynucení si spuštění finalizéru
         System.gc();                // Volání Garbage Collectoru
-        System.out.format("Počet zákazníků: %d%n", nCustomers);
-=======
-        //nutné vynucení si zrušení objektu (instance třídy Customer)
-        //na takto krátkém programu nutné spustit debugování a nastavit breakpointy
-        System.runFinalization();//vynucení  si spuštění finalizérů
-        System.gc();//volání Garbage Collectoru
         System.out.format("Počet zákazníků: %d%n", Customer.nCustomers);
->>>>>>> b840612adda681e1125e8bbb0a74896d77c39e1f
     }
 }
--- lessons/src/_10m/_11d_14/DestroyObjects.java
+++ lessons/src/_10m/_11d_14/DestroyObjects.java
@@ -1,22 +1,10 @@
 package _10m._11d_14;
-<<<<<<< HEAD
 
 /**
  * Rušení objektů
  */
 public class DestroyObjects {
 
-    public static void main(String[] args) {
-//        InstanceMethods im = new Instancemethods();
-//        im.setX(4);
-//        System.out.println(im.getX());
-//        // Zrušení objektu (instance třídy InstanceMethods) im:
-//        im = null;
-//        System.out.println(im.getX()); // objekt zrušen, výjimka se vyhodí
-    }
-=======
-//Rušení objektů
-public class DestroyObjects {
     public static void main(String[] args) {
         InstanceMethods im = new InstanceMethods();
         im.setX(4);
@@ -26,6 +14,4 @@ public class DestroyObjects {
         System.out.println(im.getX());//objekt zrušen, výjimka se vyhodí
 
     }
-
->>>>>>> b840612adda681e1125e8bbb0a74896d77c39e1f
 }
