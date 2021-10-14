bc126659573eabfc4f157cda7bbaf025553d456f
Štěpán Novák <stepan.novak@student.spsmb.cz>
2021-10-14 11:36:59 +0200

    ahoj radku

--- lessons/src/_10m/_14d_15/OkenkoGUI.java
+++ lessons/src/_10m/_14d_15/OkenkoGUI.java
@@ -1,2 +1,21 @@
-package _10m._14d_15;public class OkenkoGUI {
+package _10m._14d_15;
+
+import java.awt.*;
+public class OkenkoGUI{
+    Button helloBT;
+    Label lab;
+
+    OkenkoGUI(Frame frame) {
+        frame.setLayout(new FlowLayout());
+        this.helloBT = new Button("Hello");
+        this.lab = new Label ("Nazdar");
+        frame.add(this.helloBT);
+        frame.add(this.lab);
+        frame.setSize(170, 100);
+    }
+    public static void main(String[] args){
+        Frame bruh = new Frame("THIS");
+        OkenkoGUI okenko = new OkenkoGUI(bruh);
+        bruh.setVisible(true);
+    }
 }
---
+++ lessons/src/_10m/_14d_15/StringsN.java
@@ -0,0 +1,16 @@
+package _10m._14d_15;
+
+/**
+ * Dvě třídy pro práci s řetězci
+ * String - konstantní, nelze měnit --> rychlý
+ * StringBuffer - měnitelný -> pomalejší
+ * zásadní odlišnost od ostatních objektů - lze vyrobit instanci bez operátru new
+ * 1. prev ma index 0;
+ * Vytvoření řetězce
+ */
+public class StringsN {
+    String s1, s2, s3, s4, s5, s6, s7;
+    byte[] bajty = {(byte)'E', (byte)'v', (byte)'a'};
+    char[] znaky = {}
+}
+
