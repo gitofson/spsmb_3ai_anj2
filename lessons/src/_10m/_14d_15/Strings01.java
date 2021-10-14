ebe8997fb5d7fef15f194418f24bca0e435c20ff
stemberk <stemberk@spsmb.cz>
2021-10-14 11:25:45 +0200

    Strings01+

---
+++ lessons/src/_10m/_14d_15/Strings01.java
@@ -0,0 +1,16 @@
+package _10m._14d_15;
+
+public class Strings01 {
+    //Dvě třídy pro práci s řetězci
+    //String - konstantní, nelze měnit -> rychlý
+    //StringBuffer - měnitelný -> pomalejší
+    //zásadní odlišnost od ostatních objektů - lze vyrobit instanci bez operátoru new
+    //1. prvek má index 0;
+    //Vytvoření řetězce:
+    String s1, s2, s3, s4, s5, s6, s7;
+    byte[] bajty = {(byte)'E', (byte)'v', (byte)'a'};
+    char[] znaky = {'M', 'a', 'r', 't', 'i', 'n', 'a'};
+    //StringBuffer buf = new StringBuffer("Dobrý den");
+    //s1 = new String("Čau");
+
+}
