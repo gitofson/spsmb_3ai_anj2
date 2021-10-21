c929bbb6cfd3e562fc8d8257b792e334ee932717
Vojtech5 <vojods@seznam.cz>
2021-10-21 11:04:50 +0200

    Domácí úkol Odstrčil

--- lessons/src/_10m/_21d_17/DomaciUkol.java
+++ lessons/src/_10m/_21d_17/DomaciUkol.java
@@ -1,43 +1,8 @@
 package _10m._21d_17;
 
 public class DomaciUkol {
-/***
- * Každý sem napište metodu třídy dle zadání
-    Fiala
-    public static int obsahObdelnika(int stranaA, int stranaB)
-
-    Hoffman
-    public static int obsahPravouhlehoTrojuhelnika(int odvesnaA, int odvesnaB)
-
-    Hojný
-    public static int obvodObdelnika(int stranaA, int stranaB)
-
-    Hudeček
-    public static int obvodPravouhlehoTrojuhelnika(int odvesnaA, int odvesnaB)
-
-    Kolář
-    public static double obsahKruhu(double polomer)
-
-    Novák A.
-    public static double obvodKruhu(double polomer)
-
-    Novák Š.
-    public static int povrchKrychle(int hranaA)
-
-    Odstrčil
-    public static int objemKrychle(int hranaA)
-
-    Pelikán
-    může si vybrat svou úlohu
-
-    Rosenbaum
-    public static long faktorial(byte n)
-
-    Škrabánek
-    Výpočet fibbonaciho posloupnosti
-    private static int fibonacci(int n)
-
-    Vítek
-    public static double povrchKoule(double polomer)
- ***/
+    public static int objemKrychle(int hranaA){
+    return hranaA*hranaA*hranaA;
+    }
 }
+
