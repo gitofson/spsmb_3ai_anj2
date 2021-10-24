c23e8daaf6470727c8cc23d8a33e1dc5e7844d10
Šimon Rosenbaum <chopme@seznam.cz>
2021-10-24 13:27:16 +0200

    faktorial methoda

--- lessons/src/_10m/_21d_17/DomaciUkol.java
+++ lessons/src/_10m/_21d_17/DomaciUkol.java
@@ -3,48 +3,53 @@ package _10m._21d_17;
 import java.util.Scanner;
 
 public class DomaciUkol {
-/***
- * Každý sem napište metodu třídy dle zadání
-    Fiala
-    public static int obsahObdelnika(int stranaA, int stranaB)
+    /***
+     * Každý sem napište metodu třídy dle zadání
+     Fiala
+     public static int obsahObdelnika(int stranaA, int stranaB)
 
-    Hoffman
-    public static int obsahPravouhlehoTrojuhelnika(int odvesnaA, int odvesnaB)
+     Hoffman
+     public static int obsahPravouhlehoTrojuhelnika(int odvesnaA, int odvesnaB)
 
-    Hojný
-    public static int obvodObdelnika(int stranaA, int stranaB)
+     Hojný
+     public static int obvodObdelnika(int stranaA, int stranaB)
 
-    Hudeček
-    public static int obvodPravouhlehoTrojuhelnika(int odvesnaA, int odvesnaB)
+     Hudeček
+     public static int obvodPravouhlehoTrojuhelnika(int odvesnaA, int odvesnaB)
 
-    Kolář
-    public static double obsahKruhu(double polomer)
+     Kolář
+     public static double obsahKruhu(double polomer)
 
-    Novák A.
-    public static double obvodKruhu(double polomer)
+     Novák A.
+     public static double obvodKruhu(double polomer)
 
-    Novák Š.
-    public static int povrchKrychle(int hranaA)
+     Novák Š.
+     public static int povrchKrychle(int hranaA)
 
-    Odstrčil
-    public static int objemKrychle(int hranaA)
+     Odstrčil
+     public static int objemKrychle(int hranaA)
 
-    Pelikán
-    může si vybrat svou úlohu*/
+     Pelikán
+     může si vybrat svou úlohu
+     ***/
+    public static long faktorial(byte n) {
+        if (n == 0) return 1;
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
 
-/**
+    /***
+     * Škrabánek
+     * Výpočet fibbonaciho posloupnosti
+     * private static int fibonacci(int n)
+     *
+     * Vítek
+     * public static double povrchKoule(double polomer)
+     ***/
+    public static void main(String[] args) {
 
-    Rosenbaum
-    public static long faktorial(byte n)
-
-    Škrabánek
-    Výpočet fibbonaciho posloupnosti
-    private static int fibonacci(int n)
-
-    Vítek
-    public static double povrchKoule(double polomer)
- ***/
-public static void main(String[] args) {
-
-}
+    }
 }
