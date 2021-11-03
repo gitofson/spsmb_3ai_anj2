4859316624a5f4ec9c8e77a784e03963bb35fb7f
stemberk <stemberk@spsmb.cz>
2021-11-03 14:49:11 +0100

    UnitTesty+

---
+++ lessons/src/_11m/_03d_20/unit_testy/DomaciUkolTest.java
@@ -0,0 +1,57 @@
+package _11m._03d_20.unit_testy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DomaciUkolTest {
+
+    @Test
+    public void obsahObdelnika() {
+        assertEquals(1, DomaciUkol.obsahObdelnika(1,1));
+    }
+
+    @Test
+    public void obsahPravouhlehoTrojuhelnika() {
+    }
+
+    @Test
+    public void obvodPravouhlehoTrojuhelnika() {
+    }
+
+    @Test
+    public void objemKoule() {
+    }
+
+    @Test
+    public void obsahKruhu() {
+    }
+
+    @Test
+    public void obvodKruhu() {
+    }
+
+    @Test
+    public void povrchkrychle() {
+    }
+
+    @Test
+    public void objemKrychle() {
+    }
+
+    @Test
+    public void printBoard() {
+    }
+
+    @Test
+    public void faktorial() {
+    }
+
+    @Test
+    public void faktorialRekurzivne() {
+    }
+
+    @Test
+    public void povrchKoule() {
+    }
+}
---
+++ lessons/src/_11m/_03d_20/unit_testy/MojeCislo.java
@@ -0,0 +1,11 @@
+package _11m._03d_20.unit_testy;
+
+public class MojeCislo {
+    private int cislo;
+    public MojeCislo(int cislo){
+        this.cislo = cislo;
+    }
+    public int getCislo() {
+        return this.cislo;
+    }
+}
