ff462038e82387e1063a6d9dda24f8e056dd6bd6
stemberk <stemberk@spsmb.cz>
2021-11-11 12:02:02 +0100

    ukoly +

---
+++ lessons/src/_11m/_11d_22/cviceni/Ukoly.java
@@ -0,0 +1,9 @@
+package _11m._11d_22.cviceni;
+//Napište statickou metodu void serad(int[] x, int[] y),
+// která seřadí pole x do pole y vzestupně
+
+//Napište statickou metodu int sude(int[] x, int[] y),
+// která zkopíruje z pole x do pole y jen prvky se sudou hodnotou
+// a vrátí počet těchto prvků
+public class Ukoly {
+}
