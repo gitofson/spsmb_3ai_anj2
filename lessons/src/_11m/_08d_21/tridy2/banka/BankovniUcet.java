0e21b8342b2ff4c92867846f76d66746c2adc6f9
stemberk <stemberk@spsmb.cz>
2021-11-11 11:04:43 +0100

    bankovni ucet

---
+++ lessons/src/_11m/_08d_21/tridy2/banka/BankovniUcet.java
@@ -0,0 +1,43 @@
+package _11m._08d_21.tridy2.banka;
+
+public class BankovniUcet {
+    //standartní inicializace na 0 v Javě u proměnných instance
+    private static int pocitadloCiselUctu = 1000000;
+    private int zustatek;
+    private String vlastnikUctu;
+    private int cisloUctu;
+    public BankovniUcet(int zustatek, String vlastnikUctu){
+        this.cisloUctu = BankovniUcet.pocitadloCiselUctu;
+        this.vlastnikUctu = vlastnikUctu;
+        this.zustatek = zustatek;
+        //this.BankovniUcet by fungovalo take, ale neni to systemeticky dobře
+        BankovniUcet.pocitadloCiselUctu++;
+    }
+    public int getZustatek() {
+        return this.zustatek;
+    }
+
+    public String getVlastnikUctu() {
+        return this.vlastnikUctu;
+    }
+
+    public int getCisloUctu() {
+        return this.cisloUctu;
+    }
+
+    public boolean vyber(int castka){
+        if(castka > 0 && this.zustatek>=castka){
+            this.zustatek -= castka;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean vklad(int castka){
+        if(castka > 0 ){
+            this.zustatek += castka;
+            return true;
+        }
+        return false;
+    }
+}
