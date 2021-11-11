ffd9fe6265cafb8691e266c475b19550cd1c0306
stemberk <stemberk@spsmb.cz>
2021-11-11 12:05:21 +0100

    ukoly +

--- lessons/src/_11m/_11d_22/cviceni/Ukoly.java
+++ lessons/src/_11m/_11d_22/cviceni/Ukoly.java
@@ -6,4 +6,12 @@ package _11m._11d_22.cviceni;
 // která zkopíruje z pole x do pole y jen prvky se sudou hodnotou
 // a vrátí počet těchto prvků
 public class Ukoly {
+    public static void serad(int[] x, int[] y){
+
+    }
+    public  static int sude(int[] x, int[] y){
+        int out = 0;
+
+        return out;
+    }
 }
