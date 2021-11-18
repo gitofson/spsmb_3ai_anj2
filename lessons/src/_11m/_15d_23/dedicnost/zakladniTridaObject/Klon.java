fdb2095a612bd08243c75be92f97ed248b522f07
stemb <stemberk@spsmb.cz>
2021-11-18 08:43:54 +0100

    Klon +

--- lessons/src/_11m/_15d_23/dedicnost/zakladniTridaObject/Klon.java
+++ lessons/src/_11m/_15d_23/dedicnost/zakladniTridaObject/Klon.java
@@ -1,8 +1,9 @@
 package _11m._15d_23.dedicnost.zakladniTridaObject;
 
 public class Klon implements  Cloneable {
-    int i;
+    private int i;
     Klon(int i ) {this.i = i;}
+
     //překrytí metody clone()
     protected Object clone() {
         Klon k = null;
@@ -13,5 +14,15 @@ public class Klon implements  Cloneable {
         catch(CloneNotSupportedException ex) {
             ex.printStackTrace();
         }
+        return k;
+    }
+    public int getI() {
+        return i;
+    }
+    public static void main(String[] args) {
+        Klon kopie, orig = new Klon(5);
+        //vytvoření instance, resp. kopie instance orig (Třída Klon) bez operátoru new
+        kopie = (Klon) orig.clone();
+        System.out.format("Original:%d%nKopie:%d%n",orig.getI(), kopie.getI() );
     }
 }
