fa24bcf946745a82bd8b0e9121be05fd2c645ede
adam.novak <dontstarvecz@gmail.com>
2021-11-25 11:23:24 +0100

    Ukol - Adam

--- lessons/src/_11m/_18d_24/cviceniKolo/HorskeKolo.java
+++ lessons/src/_11m/_18d_24/cviceniKolo/HorskeKolo.java
@@ -4,9 +4,24 @@ public class HorskeKolo extends Kolo {
     private int prevodyVpredu;
     private int prevodyVzadu;
 
-    public HorskeKolo(String vyrobce, int cena, int prevodyVpredu, int prevodyVzadu) {
+    public HorskeKolo(String vyrobce, int cena, boolean maPrehazovacku, int prevodyVpredu, int prevodyVzadu) {
         super(vyrobce, cena, true);
         this.prevodyVpredu = prevodyVpredu;
         this.prevodyVzadu = prevodyVzadu;
+    }
+
+    @Override
+    public void vypisCoJsiZac() {
+        super.vypisCoJsiZac();
+        System.out.println(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "HorskeKolo{" +
+                "prevodyVpredu=" + prevodyVpredu +
+                ", prevodyVzadu=" + prevodyVzadu +
+                ", druh='" + druh + '\'' +
+                '}';
     }
 }
--- lessons/src/_11m/_18d_24/cviceniKolo/Kolo.java
+++ lessons/src/_11m/_18d_24/cviceniKolo/Kolo.java
@@ -1,10 +1,13 @@
 package _11m._18d_24.cviceniKolo;
+
+import _11m._18d_24.cviceniVozidlo.JednostopeVozidlo;
+
 // Vytvořte třídu Kolo s proměnnými vyrobce, cena a boolean maPrehazovacku.
 // Doplňte do této třídy příslušné metody a konstruktory. Děděním vytvořte
 // třídu HorskéKolo, kterému doplňte členské proměnné int prevodyVzadu,
 // prevodyVpredu. Zajistěte, aby konstruktor správně vytvářel objekty této
 // třídy (např. proměnná maPrehazovacku musí být vždy true).
-public class Kolo {
+public class Kolo extends JednostopeVozidlo {
     private String vyrobce;
     private int cena;
     private boolean maPrehazovacku;
@@ -22,6 +25,21 @@ public class Kolo {
     }
     public int getCena(){
         return cena;
+    }
+
+    @Override
+    public String toString() {
+        return "Kolo{" +
+                "vyrobce='" + vyrobce + '\'' +
+                ", cena=" + cena +
+                ", maPrehazovacku=" + maPrehazovacku +
+                ", druh='" + druh + '\'' +
+                '}';
+    }
+
+    @Override
+    public void vypisCoJsiZac() {
+        System.out.println(this.toString());
     }
 }
 
