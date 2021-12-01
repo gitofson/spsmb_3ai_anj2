d13bcae85e6f73f66d6b68d433b32feea1c4055e
Adam Novák <dontstarvecz@gmail.com>
2021-12-01 19:52:44 +0100

    DÚ

---
+++ lessons/src/_11m/_22d_25/cviceni/c2/TestDutyCtverec.java
@@ -0,0 +1,8 @@
+package _11m._22d_25.cviceni.c2;
+
+public class TestDutyCtverec {
+    public static void main(String[] args) {
+        DutyCtverec dc = new DutyCtverec(9);
+        (dc).zobraz();
+    }
+}
