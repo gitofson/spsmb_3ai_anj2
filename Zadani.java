6a02bf04e8589aa239254d070a45301a7dfb2811
Jan Vítek <86429043+Hanis03@users.noreply.github.com>
2021-12-01 19:28:49 +0100

    cviceni1

---
+++ Zadani.java
@@ -0,0 +1,9 @@
+package _11m._22d_25.cviceni.c1;
+//Vytvořte třídy Ctverec a Usecka s vhodně zvolenými metodami a proměnnými.
+//Vytvořte rozhraní Zobrazitelný s metodou void zobraz(). Zajistěte, aby třídy
+//Ctverec a Usecka implementovaly toto rozhraní tak, že se vykreslí na obrazovku např.:
+//      ***
+//      ***             *************
+//      ***
+public class Zadani {
+}
