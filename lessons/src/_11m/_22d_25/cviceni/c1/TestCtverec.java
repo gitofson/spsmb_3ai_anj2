ba61121deb02fab9745203f7a782ed13ee170189
SH4RK7 <92031517+SH4RK7@users.noreply.github.com>
2021-12-01 23:11:12 +0100

    DU

---
+++ lessons/src/_11m/_22d_25/cviceni/c1/TestCtverec.java
@@ -0,0 +1,12 @@
+package _11m._22d_25.cviceni.c1;
+
+public class TestCtverec {
+    public static void main(String[] args) {
+        Ctverec c =  new Ctverec(9);
+        Usecka u = new Usecka(10);
+        (c).zobraz();
+        (u).zobraz();
+
+
+    }
+}
