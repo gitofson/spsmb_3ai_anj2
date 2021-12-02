c4e2e4b4871aea939f2c859aa60e8e6266f4f915
stemberk <stemberk@spsmb.cz>
2021-12-02 12:15:02 +0100

    DU +

---
+++ lessons/src/_12m/_02d_28/kolekce/Ukol.java
@@ -0,0 +1,7 @@
+package _12m._02d_28.kolekce;
+//ArrayList lze vytvořit i s předem vytvořeným počtem prvků. Takto:
+//ar = new ArrayList<Integer>(N). Proveďte rychlostní test a srovnejte
+//s ar = new ArrayList<Integer> pro milión prvků;
+public class Ukol {
+
+}
