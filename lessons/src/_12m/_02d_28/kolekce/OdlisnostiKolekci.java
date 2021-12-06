fe34fb864cbbcea4cff6e975b960d4961b39ebed
stemberk <stemberk@spsmb.cz>
2021-12-06 11:21:03 +0100

    sublist+

--- lessons/src/_12m/_02d_28/kolekce/OdlisnostiKolekci.java
+++ lessons/src/_12m/_02d_28/kolekce/OdlisnostiKolekci.java
@@ -6,18 +6,21 @@ import java.util.HashSet;
 
 public class OdlisnostiKolekci {
     public static void main(String[] args) {
+        //seznam
         ArrayList<String> ar = new ArrayList<String>();
         ar.add("prvni");
         ar.add("druhy");
         ar.add("prvni");
         System.out.println("ArrayList: " + ar);
 
+        //množina
         HashSet<String> hs = new HashSet<String>();
         hs.add("prvni");
         hs.add("druhy");
         hs.add("prvni");
         System.out.println("HashSet: " + hs);
 
+        //mapa (asiciativní pole)
         HashMap<String, String> hm = new HashMap<String, String>();
         hm.put("prvni","objekt");
         hm.put("druhy","objekt");
