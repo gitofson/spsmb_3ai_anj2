919a70faf6d1e26cbcb2ea993dde06af4909a6ac
Jan Vítek <86429043+Hanis03@users.noreply.github.com>
2021-12-13 11:20:55 +0100

    Delete lessons/src/_11m directory

--- lessons/src/_11m/_15d_23/dedicnost/Uvod.java
+++
@@ -1,7 +0,0 @@
-package _11m._15d_23.dedicnost;
-//Dědičnost - možnost přidat k bázové třídě,
-// základní třídě, rodičovské třídě, supertřídě, rodiči, předkovi
-// další vlastnosti a odvodit tak další (zděděnou) třídu, resp.
-// potomka, resp. dceřinnou třídu.
-public class Uvod {
-}
--- lessons/src/_11m/_22d_25/cviceni/c2/Test.java
+++
@@ -1,8 +0,0 @@
-package _11m._22d_25.cviceni.c2;
-
-public class Test {
-    public static void main(String[] args) {
-        dutyCtverec d = new dutyCtverec(6);
-        d.zobraz();
-    }
-}
