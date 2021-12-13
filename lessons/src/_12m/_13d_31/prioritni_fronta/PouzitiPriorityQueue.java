dd7c5ace312cb6dcaeb6d7cef27d859e63e7a0c6
stemberk <stemberk@spsmb.cz>
2021-12-13 11:00:27 +0100

    prioritni fronta

---
+++ lessons/src/_12m/_13d_31/prioritni_fronta/PouzitiPriorityQueue.java
@@ -0,0 +1,21 @@
+package _12m._13d_31.prioritni_fronta;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PouzitiPriorityQueue {
+    public static void main(String[] args) {
+        Queue<Vojak> kantyna = new PriorityQueue<Vojak>();
+
+        kantyna.add(new Vojak(1, "major", "letectvo"));
+        kantyna.add(new Vojak(3, "plukovnik", "pechota"));
+        kantyna.add(new Vojak(2, "podplukovnik", "policie"));
+        kantyna.add(new Vojak(3, "plukovnik", "spojari"));
+        kantyna.add(new Vojak(4, "nadplukovnik", "chemici"));
+        System.out.println("Fronta najednou: " + kantyna);
+        System.out.println("\nPoradi vydeje jidel:");
+        while (kantyna.isEmpty() == false){
+            System.out.println(kantyna.remove());
+        }
+    }
+}
---
+++ lessons/src/_12m/_13d_31/prioritni_fronta/Vojak.java
@@ -0,0 +1,35 @@
+package _12m._13d_31.prioritni_fronta;
+
+public class Vojak implements Comparable<Vojak>{
+    private int pocetHvezdicek;
+    private String hodnost;
+    private String typVojska;
+
+    /*public int getPocetHvezdicek() {
+        return pocetHvezdicek;
+    }*/
+
+    public void setPocetHvezdicek(int pocetHvezdicek) {
+        this.pocetHvezdicek = pocetHvezdicek;
+    }
+
+    public Vojak(int pocetHvezdicek, String hodnost, String typVojska) {
+        this.pocetHvezdicek = pocetHvezdicek;
+        this.hodnost = hodnost;
+        this.typVojska = typVojska;
+    }
+
+    @Override
+    public int compareTo(Vojak o) {
+        return o.pocetHvezdicek - this.pocetHvezdicek;
+    }
+
+    @Override
+    public String toString() {
+        return "Vojak{" +
+                "pocetHvezdicek=" + pocetHvezdicek +
+                ", hodnost='" + hodnost + '\'' +
+                ", typVojska='" + typVojska + '\'' +
+                '}';
+    }
+}
