78cf474c4a1e0eec4110e43cb71792a94cccbc91
stemberk <stemberk@spsmb.cz>
2021-12-16 11:55:49 +0100

    odstraneni duplicit pomoci konstruktoru mnoziny a seznamu

--- lessons/src/_12m/_16d_32/EliminaceDuplicit.java
+++ lessons/src/_12m/_16d_32/EliminaceDuplicit.java
@@ -3,6 +3,7 @@ package _12m._16d_32;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 
 public class EliminaceDuplicit {
     public static void main(String[] args) {
@@ -11,5 +12,9 @@ public class EliminaceDuplicit {
         d.add("druhy");
         d.add("prvni");
         System.out.println("Duplicitni: "+d);
+        //Toto je celý trik
+        Collection<String> nd = new ArrayList<>(new HashSet<>(d));
+
+        System.out.println("Neduplicitni: " + nd);
     }
 }
