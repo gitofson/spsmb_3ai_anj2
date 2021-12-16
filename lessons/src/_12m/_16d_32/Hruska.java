b4e26997c07100a3493ffb050bf64fe1447a90f8
stemberk <stemberk@spsmb.cz>
2021-12-16 12:06:28 +0100

    odstraneni duplicit pomoci konstruktoru mnoziny a seznamu

---
+++ lessons/src/_12m/_16d_32/Hruska.java
@@ -0,0 +1,36 @@
+package _12m._16d_32;
+
+public class Hruska implements Comparable<Hruska>{
+    private int cena;
+    public Hruska(int cena) {
+        this.cena = cena;
+    }
+
+    @Override
+    public int compareTo(Hruska o) {
+        if(this.cena > o.cena) { return +1; }
+        else if (cena == o.cena ) {return 0; }
+        else return -1;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.cena;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this) {
+            return true;
+        }
+        if(obj instanceof Hruska == false) {
+            return false;
+        }
+        return (cena == ((Hruska) obj).cena);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d", this.cena);
+    }
+}
