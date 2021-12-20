9bfc34baf3351e1c82e2f20a0f2b68f0ec47ca26
stemberk <stemberk@spsmb.cz>
2021-12-20 12:15:46 +0100

    iterátory v mapě

---
+++ lessons/src/_12m/_20d_33/NastaveniVMape.java
@@ -0,0 +1,23 @@
+package _12m._20d_33;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Praktické použití mapy
+public class NastaveniVMape {
+    private static String[] key = {"pozadi", "popredi", "ramecek"};
+    private static String[] hodDef = {"bila", "cerna", "modra"};
+    private static String[] hodUziv = {null, "modra", "cervena"};
+
+    static Map<String, String> operations(String[] hodnoty ) {
+        Map<String, String> m = new HashMap<>();
+
+        for (int i = 0; i < key.length; i++) {
+            if(hodnoty[i] != null) {
+                m.put(key[i], hodnoty[i]);
+            }
+        }
+        return m;
+    }
+
+}
