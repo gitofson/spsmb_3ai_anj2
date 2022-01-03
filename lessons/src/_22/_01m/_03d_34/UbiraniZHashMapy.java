63562666660a054e481042a97b12571e6f92d649
stemberk <stemberk@spsmb.cz>
2022-01-03 14:28:01 +0100

    UbiraniZHashMapy

---
+++ lessons/src/_22/_01m/_03d_34/UbiraniZHashMapy.java
@@ -0,0 +1,27 @@
+package _22._01m._03d_34;
+
+import _12m._20d_33.Vaha;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class UbiraniZHashMapy {
+    static void naplneniMapyATisk(HashMap<String, Vaha> hm) {
+        hm.clear();
+        for (int i = 1; i <= 4 ; i++) {
+            String jmeno = "Robot" + i;
+            hm.put(jmeno, new Vaha(50 + i));
+        }
+        System.out.println("Mapa: " + hm);
+    }
+    public static void main(String[] args) {
+        HashMap<String, Vaha> hm = new HashMap<>();
+        naplneniMapyATisk(hm);
+        Set<String> kl = hm.keySet();
+        //odstranění prvku z mapy podle klíče:
+        kl.remove("Robot1");
+        kl.remove("Robot2");
+        System.out.println("Mapa bez Robot1,2: " + hm);
+
+    }
+}
