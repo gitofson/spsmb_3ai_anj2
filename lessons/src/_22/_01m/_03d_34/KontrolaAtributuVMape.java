fc55622728703e58aaa63fad6e454dd19bb142fc
stemberk <stemberk@spsmb.cz>
2022-01-03 15:31:03 +0100

    porovnani map

---
+++ lessons/src/_22/_01m/_03d_34/KontrolaAtributuVMape.java
@@ -0,0 +1,16 @@
+package _22._01m._03d_34;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class KontrolaAtributuVMape {
+    public static void main(String[] args) {
+        String[] key = {"pozadi", "popredi", "ramecek"};
+        HashSet<String> atributy = new HashSet<>(Arrays.asList(key));
+        HashMap<String, String> nastaveni = new HashMap<>();
+        nastaveni.put("pozadi", "bila");
+
+
+    }
+}
--- lessons/src/_22/_01m/_03d_34/NastaveniVMape.java
+++ lessons/src/_22/_01m/_03d_34/NastaveniVMape.java
@@ -1,7 +1,9 @@
 package _22._01m._03d_34;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 //Praktické použití mapy
 public class
@@ -12,7 +14,7 @@ NastaveniVMape {
     private static String[] hodDef = {"bila", "cerna", "modra"};
     private static String[] hodUziv = {null, "modra", "cervena"};
 
-    static Map<String, String> operations(String[] hodnoty ) {
+    static Map<String, String> options(String[] hodnoty ) {
         Map<String, String> m = new HashMap<>();
 
         for (int i = 0; i < key.length; i++) {
@@ -21,6 +23,16 @@ NastaveniVMape {
             }
         }
         return m;
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> defaultNastaveni = options(hodDef);
+        Map<String, String> uzivatelNastaveni = options(hodUziv);
+        Map<String, String> platneNastaveni = new HashMap<>(defaultNastaveni);
+        platneNastaveni.putAll(uzivatelNastaveni);
+        System.out.println("Default:  " + defaultNastaveni);
+        System.out.println("Uzivatel: " + uzivatelNastaveni);
+        System.out.println("Platne:   " + platneNastaveni);
     }
 
 }
