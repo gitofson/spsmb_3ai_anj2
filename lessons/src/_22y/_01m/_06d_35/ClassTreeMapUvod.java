57a94eeb3f4e4ff8242c5041806059c596c1a7e4
stemberk <stemberk@spsmb.cz>
2022-01-06 11:29:03 +0100

    TreeMap

--- lessons/src/_22y/_01m/_06d_35/ClassTreeMapUvod.java
+++ lessons/src/_22y/_01m/_06d_35/ClassTreeMapUvod.java
@@ -14,6 +14,19 @@ package _22y._01m._06d_35;
 // TreeMap(Comparator<? super K> c) komparátor pro absolutní řazení nastavíme
 
 //Další metody ve třídě TreeMap:
-//SortedMap<K,V> headMap(K doKlice) -
+// SortedMap<K,V> headMap(K doKlice) - vrací podmapu, pro kterou platí, že klíče jsou
+// ostře menší než daný klíč.
+// SortedMap<K,V> tailMap(K odKliceVcetne) - vrací podmapu, pro kterou platí, že klíče jsou
+// větší, nebo rovny danému klíči.
+// SortedMap<K,V> subMap(K odKliceVcetne, K doKlice) - umožňuje získat podmapu
+// "z prostředka" stávající mapy.
+
+class VahaKlic implements Comparable<VahaKlic> {
+    double vaha;
+    VahaKlic(double vaha) {
+        this.vaha = vaha;
+    }
+
+}
 public class ClassTreeMapUvod {
 }
