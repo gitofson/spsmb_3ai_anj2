9b4c198d15c0c95b9e248773c3598b0e78b552de
stemberk <stemberk@spsmb.cz>
2022-01-06 11:43:09 +0100

    TreeMap

--- lessons/src/_22y/_01m/_06d_35/ClassTreeMapUvod.java
+++ lessons/src/_22y/_01m/_06d_35/ClassTreeMapUvod.java
@@ -21,12 +21,69 @@ package _22y._01m._06d_35;
 // SortedMap<K,V> subMap(K odKliceVcetne, K doKlice) - umožňuje získat podmapu
 // "z prostředka" stávající mapy.
 
+import java.util.Comparator;
+import java.util.TreeMap;
+
 class VahaKlic implements Comparable<VahaKlic> {
     double vaha;
+
+
+
     VahaKlic(double vaha) {
         this.vaha = vaha;
     }
 
+    @Override
+    public int compareTo(VahaKlic o) {
+        return ((int) (this.vaha - o.vaha));
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) this.vaha;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        //stejná instance ?
+        if (obj == this){
+            return true;
+        }
+        //je vstupní objekt instancí, nebo potomkem třídy VahaKlic?
+        //Ano - pokračujeme
+        if(obj instanceof VahaKlic == false) {
+            return false;
+        }
+        return (vaha == ((VahaKlic) obj).vaha);
+    }
+}
+class VahovyKomparator implements Comparator<VahaKlic> {
+    @Override
+    public int compare(VahaKlic o1, VahaKlic o2) {
+        return (int) (o1.vaha - o2.vaha);
+    }
 }
 public class ClassTreeMapUvod {
+    static void praceSMapou(TreeMap<VahaKlic, String> tm) {
+        String s;
+        Comparator<? super VahaKlic> c = tm.comparator();
+        if(c == null) {
+            s = "prirozene razeni";
+        } else {
+            s = c.getClass().getName();
+        }
+        System.out.println("Komparator: " + s);
+
+        tm.put(new VahaKlic(85), "Pavel");
+        tm.put(new VahaKlic(105), "Venca");
+        tm.put(new VahaKlic(74), "Karel");
+        System.out.println("Mapa: " + tm);
+
+        System.out.println("Nejvice Vazi: ");
+        System.out.println("Dva nejtezsi: ");
+    }
+    public static void main(String[] args) {
+        praceSMapou(new TreeMap<VahaKlic, String>());
+        praceSMapou(new TreeMap<VahaKlic, String>(new VahovyKomparator()));
+    }
 }
