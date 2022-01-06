7c8dc92345badf611f486685810af9bea6cb643e
stemberk <stemberk@spsmb.cz>
2022-01-06 10:55:55 +0100

    kontrola atributu ukol

--- lessons/src/_22/_01m/_03d_34/KontrolaAtributuVMape.java
+++ lessons/src/_22/_01m/_03d_34/KontrolaAtributuVMape.java
@@ -10,6 +10,13 @@ public class KontrolaAtributuVMape {
         HashSet<String> atributy = new HashSet<>(Arrays.asList(key));
         HashMap<String, String> nastaveni = new HashMap<>();
         nastaveni.put("pozadi", "bila");
+        //neznamy atribut
+        nastaveni.put("text", "zelena");
+        System.out.println("Seznam atributu:   " + atributy);
+        System.out.println("Seznam nastaveni:  " + nastaveni);
+        HashSet<String> chybi = new HashSet<>(atributy);
+
+        System.out.println("Atributy chybi:    " + chybi);
 
 
     }
