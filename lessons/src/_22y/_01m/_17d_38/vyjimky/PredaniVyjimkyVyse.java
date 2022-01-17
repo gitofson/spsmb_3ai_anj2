f0d1c19d2a6a92adb734dfcdd5d60a8370776f96
stemberk <stemberk@spsmb.cz>
2022-01-17 14:40:21 +0100

    VyjimkyUvod

---
+++ lessons/src/_22y/_01m/_17d_38/vyjimky/PredaniVyjimkyVyse.java
@@ -0,0 +1,9 @@
+package _22y._01m._17d_38.vyjimky;
+
+// Jedná se o tzv. propagaci, resp. šíření výjimek. Metoda, kde se výjimka ocitla
+// se zříká odpovědnosti za její zpracování. Toto předání
+// odpovědnosti se deklaruje  již v hlavičce metody pomocí klíčového
+// slova throws. Viz případ dole. Řešení problému, co s vzniklou
+// výjimkou se tak odsouvá na později...
+public class PredaniVyjimkyVyse {
+}
