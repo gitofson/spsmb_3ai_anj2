a25e3aa295f55e07c7be24ed807a3877dc107ca7
stemberk <stemberk@spsmb.cz>
2022-01-20 11:34:28 +0100

    AssertDemo

---
+++ lessons/src/_22y/_01m/_20d_39/vyjimky/AssertDemo.java
@@ -0,0 +1,18 @@
+package _22y._01m._20d_39.vyjimky;
+
+public class AssertDemo {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("i = " + i + ": ");
+        try{
+            assert(i == 0)  //Test splnění požadované podmínky
+                : "\nPříkaz assert vyhodil výjimku"; // Zpráva při nespnění
+        }
+        catch (AssertionError ae){
+            System.out.println("Byla vyhozena výjimka " + ae);
+            System.exit(0);
+        }
+        System.out.println("Nebyla vyhozena žádná výjimka");
+        System.exit(0);
+    }
+}
