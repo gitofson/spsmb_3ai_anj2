206764e8769ed2bf92f1f043f07e84d58e429e4d
stemberk <stemberk@spsmb.cz>
2022-01-31 14:29:00 +0100

    Zadani

--- lessons/src/_22y/_01m/_31d_41/ukol/Zadani.java
+++ lessons/src/_22y/_01m/_31d_41/ukol/Zadani.java
@@ -7,5 +7,25 @@ package _22y._01m._31d_41.ukol;
 // 4. Modifikujte předchozí program tak, že budou vypisovány i soubory ze všech vnořených adresářů
 //    z těchto podadresářů.
 
+import java.io.File;
+import java.io.FilenameFilter;
+
 public class Zadani {
+    public static void main(String[] args) {
+        File f = new File("C:\\");
+        File[] fileList = f.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return (new File(dir, name)).isDirectory();
+            }
+        });
+        //totéž  s lambda operátorem "čtyřtečka" ::
+        //File[] fileList = f.listFiles(File::isDirectory);
+        for (File file : fileList) {
+            System.out.println(file.getName());
+        }
+    }
+
+
+
 }
