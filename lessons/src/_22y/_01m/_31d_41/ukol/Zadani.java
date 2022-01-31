5a995444da7d9cd9928ad38654d1eed6e36cf8b3
stemberk <stemberk@spsmb.cz>
2022-01-31 14:17:48 +0100

    Zadani

---
+++ lessons/src/_22y/_01m/_31d_41/ukol/Zadani.java
@@ -0,0 +1,11 @@
+package _22y._01m._31d_41.ukol;
+// 1. Napište program, který vypisuje jména všech podadresářů v zadaném rodičovském adresáři.
+// 2. Modifikujte předchozí program tak, že budou vypisovány i všechny vnořené adresáře
+//    z těchto podadresářů.
+// 3. Napište program, který vypíše podle zadané masky všechny soubory, které se nacházejí v zadaném
+//    rodičovském adresáři.
+// 4. Modifikujte předchozí program tak, že budou vypisovány i soubory ze všech vnořených adresářů
+//    z těchto podadresářů.
+
+public class Zadani {
+}
