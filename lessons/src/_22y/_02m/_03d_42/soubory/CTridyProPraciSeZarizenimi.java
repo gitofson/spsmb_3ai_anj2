033624ac8c16212402e4f00864b6923b17112eac
stemberk <stemberk@spsmb.cz>
2022-02-03 11:22:36 +0100

    SortedSet - metody rozhraní

---
+++ lessons/src/_22y/_02m/_03d_42/soubory/CTridyProPraciSeZarizenimi.java
@@ -0,0 +1,16 @@
+package _22y._02m._03d_42.soubory;
+
+// zařízení                     přesun znaků                        přesun bytů
+//-------------------------------------------------------------------------------------
+// paměť                        CharArrayReader             ByteArrayInputStream
+//                              CharArrayWriter             ByteArrayOutputStream
+//                              StringReader                StringBufferInputStream
+//                              StringWriter
+//
+// soubor                       FileReader                  FileInputStream
+// (file)                       FileWriter                  FileOutpuStream
+//
+// roura                        PipedReader                 PipedInputStream
+// (pipe)                       PipedWriter                 PipedOutoputStream
+public class CTridyProPraciSeZarizenimi {
+}
---
+++ lessons/src/_22y/_02m/_03d_42/soubory/DTridyVlastnosti.java
@@ -0,0 +1,15 @@
+package _22y._02m._03d_42.soubory;
+
+// Třídy vlastností (filtrů) slouží pro úpravu dat, která již byla fyzicky načtena,
+// případně dat, kerá budou v zápětí fyzicky zypisována. "Nabalují" se na třídy,
+// které provádí fyzické čtení/zápis dat. Též "vlastnostní" třídy.
+
+//BufferdReader, BufferedWriter, BufferedInputStream, BufferedOutputStream  - využití vyrovnávací
+//          paměti - bufferování.
+//PushbackReader, PushbackInputStream       - vrácení načteného znaku zpět do vstupního proudu
+//PrintWriter, PrintStream                  - možnost formátováného výstupu
+//DataInputStream, DataOutputStream         - možnost čtení, či zápisu základních datových typů
+//ObjectInputStream, ObjectOutputStream     - binární čtení nebo zápis libovolných objektů
+
+public class DTridyVlastnosti {
+}
