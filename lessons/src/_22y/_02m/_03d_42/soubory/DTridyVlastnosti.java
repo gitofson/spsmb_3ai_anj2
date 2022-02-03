24f7143e929f5fbad262cec9a3aa13a0ecc21650
stemberk <stemberk@spsmb.cz>
2022-02-03 11:39:24 +0100

    EDalsiDovednostiSeSoubory

--- lessons/src/_22y/_02m/_03d_42/soubory/DTridyVlastnosti.java
+++ lessons/src/_22y/_02m/_03d_42/soubory/DTridyVlastnosti.java
@@ -1,7 +1,7 @@
 package _22y._02m._03d_42.soubory;
 
 // Třídy vlastností (filtrů) slouží pro úpravu dat, která již byla fyzicky načtena,
-// případně dat, kerá budou v zápětí fyzicky zypisována. "Nabalují" se na třídy,
+// případně dat, kerá budou v zápětí fyzicky zapisována. "Nabalují" se na třídy,
 // které provádí fyzické čtení/zápis dat. Též "vlastnostní" třídy.
 
 //BufferdReader, BufferedWriter, BufferedInputStream, BufferedOutputStream  - využití vyrovnávací
