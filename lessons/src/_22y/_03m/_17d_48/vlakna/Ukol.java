36679ec19cec27bad018a0bbc1485989106ffba6
stemberk <stemberk@spsmb.cz>
2022-03-17 11:48:36 +0100

    fix

--- lessons/src/_22y/_03m/_17d_48/vlakna/Ukol.java
+++ lessons/src/_22y/_03m/_17d_48/vlakna/Ukol.java
@@ -4,5 +4,6 @@ package _22y._03m._17d_48.vlakna;
 // čísly typu double od -1 do 1. Pokud je bod uvnitř vepsané kružnice, platí x^2+Y^2<=1.
 // Konstantu pi spočítáte jako pi = 4*No/N, kde N je celkový počet hodů a No je pošet hodů do vepsaného kruhu.
 // Můžete se inspirovat z resources/examples/c
+// Použijte co největší počet hodů a vlákna pro urychlení výpočtu.
 public class Ukol {
 }
