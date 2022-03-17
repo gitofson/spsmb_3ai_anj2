09051b00a556499a76d66d4529995eb09737dc76
stemberk <stemberk@spsmb.cz>
2022-03-17 11:45:40 +0100

    fix

---
+++ lessons/src/_22y/_03m/_17d_48/vlakna/Ukol.java
@@ -0,0 +1,8 @@
+package _22y._03m._17d_48.vlakna;
+// Vypočítejte konstantu pi pomocí modelu hodu šipkami do čtverce 2x2 o obsahu 4.
+// Zjistěte, zda je hod šipkou uvnitř vepsané jednotkové kružnice tohoto čtverce. Hod šipkou je reprezentován náhodnými
+// čísly typu double od -1 do 1. Pokud je bod uvnitř vepsané kružnice, platí x^2+Y^2<=1.
+// Konstantu pi spočítáte jako pi = 4*No/N, kde N je celkový počet hodů a No je pošet hodů do vepsaného kruhu.
+// Můžete se inspirovat z resources/examples/c
+public class Ukol {
+}
