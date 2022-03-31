c9e2bfb118d03df4a97e97d721d2143d00471e0d
stemberk <stemberk@spsmb.cz>
2022-03-31 11:25:11 +0200

    DSynchronizaceVlaken

---
+++ lessons/src/_22y/_03m/_28d_51/vlakna_producent_konzument/Ukol.java
@@ -0,0 +1,5 @@
+package _22y._03m._28d_51.vlakna_producent_konzument;
+// Napište program, který bude řešit úlohu producent - konzument pomocí vláken,
+// přičemž vlákna budou spolu komunikovat pomocí roury.
+public class Ukol {
+}
