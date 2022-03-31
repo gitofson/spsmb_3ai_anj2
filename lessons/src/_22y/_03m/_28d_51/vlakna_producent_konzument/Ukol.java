3beb9abd0c70d562c59909e37c6609e31ec94b65
stemberk <stemberk@spsmb.cz>
2022-03-31 11:34:02 +0200

    Ukol

--- lessons/src/_22y/_03m/_28d_51/vlakna_producent_konzument/Ukol.java
+++ lessons/src/_22y/_03m/_28d_51/vlakna_producent_konzument/Ukol.java
@@ -1,5 +1,6 @@
 package _22y._03m._28d_51.vlakna_producent_konzument;
 // Napište program, který bude řešit úlohu producent - konzument pomocí vláken,
-// přičemž vlákna budou spolu komunikovat pomocí roury.
+// přičemž vlákna budou spolu komunikovat pomocí roury. Příklad s rourou
+// najdete v balíčku _22y._02m._10d_44.soubory
 public class Ukol {
 }
