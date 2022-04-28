ed90a9584ad177714e48ce8fd4d6a06dc429e2f4
stemberk <stemberk@spsmb.cz>
2022-04-28 14:00:39 +0200

    lambda

--- lessons/src/_22y/_04m/lambda/AUvod.java
+++ lessons/src/_22y/_04m/lambda/AUvod.java
@@ -6,5 +6,78 @@ package _22y._04m.lambda;
 // Lambda výrazy nahrazují anonymní třídy implementující funkční rozhraní, tj. rozhraní
 // deklarující právě jednu abstraktní metodu. Lambda výrazy se pak vydávají za instance
 // tohoto rozhraní a jimi reprezentovaný kód se spustí tak, že se příslušná metoda zavolá.
+
+// Funkční  rozhraní mají deklarovánu právě jednu abstraktní metodu. Počet implicitních a
+// statických metod u nich není omezen. Pokud chceme mít jistotu, že jde opravdu o funční
+// rozhraní, anotujeme jo pomocí @FunctionalInterface.
+
+// V knihovně java.util.function je definováno 43 obecných rozhraní. Jsou rozděleny do následujících
+// skupin:
+// Konzumenti (Consumers) - zpracují parametry a nic nevrátí:
+
+// Jednoparametričtí:
+// Consumer<T>                      void accepd(T       value)
+// DoubleConsumer                   void accept(double  value)
+// IntConsumer                      void accept(int     value)
+// LongConsumer                     void accept(long    value)
+
+// Dvouparametričtí:
+// BiConsumer<T>                    void accept(T t, U      value)
+// ObjDoubleConsumer                void accept(T t, double value)
+// ObjIntConsumer                   void accept(T t, int    value)
+// ObjLongConsumer                  void accept(T t, long   value)
+
+// Producenti (Suppliers) - nepotřebují žádné argumenty, naopak vrátí hodnotu zadaného typu
+// Supplier<T>              T       get()
+// BooleanSupplier          boolean getAsBoolean()
+// DoubleSupplier           double  getAsDouble()
+// IntSupplier              int     getAsInt()
+// LongSupplier             long    getAsLong()
+
+// Funkce (Functions) - zpracují argument a vrátí hodnotu:
+// Jednoparametrické vracející Objekt
+// Function             <T, R>      R apply(T       value)
+// DoubleFunction       <   R>      R apply(double  value)
+// IntFunction          <   R>      R apply(int     value)
+// LongFunction         <   R>      R apply(long    value)
+
+// Jednparametrické vracející hodnotu primitivního typu
+// ToIntFunction        <T>         int     applyAsInt   (T      value)
+// ToDoubleFunction     <T>         double  applyAsDouble(T      value)
+// ToLongFunction       <T>         long    applyAsLong  (T      value)
+// DoubleToInt                      int     applyAsInt   (double value)
+// DoubleToLong                     long    applyAsLong  (double value)
+// IntToDoubleFunction              double  applyAsDouble(int    value)
+// IntToLongFunction                long    applyAsLong  (int    value)
+// LongToDoubleFunction             double  applyAsDouble(long   value)
+// LongToIntFunction                int     applyAsDouble(long   value)
+
+// Dvouparametrické
+// BiFunction           <T, U, R>   R       apply         (T t, U u)
+// ToDoubleBiFunction   <T, U>      double  applyAsDouble (T t, U u)
+// ToIntBiFunction      <T, U>      int     applyAsInt    (T t, U u)
+// ToLongBiFunction     <T, U>      long    applyAsLong   (T t, U u)
+
+// Unární operátory (UnaryOperators) - definovány pomocí funkcí, jejichž návratová hodnota je stejného
+// typu, jako parametr.
+// UnaryOperator<T>     T       apply        (T      value)
+// DoubleUnaryOperator  double  applyAsDouble(double value)
+// IntUnaryOperator     int     applyAsInt   (double value)
+// LongUnaryOperator    long    applyAsLong  (double value)
+
+// Binární operátory (BinaryOperators) - definovány pomocí dvouparametrických funkcí, kde typy parametrů
+// i návratový typ jsou shodné.
+// BinaryOperator<T>     T       apply        (T      left, T      right)
+// DoubleBinaryOperator  double  applyAsDouble(double left, double right)
+// IntBinaryOperator     int     applyAsInt   (int    left, int    right)
+// LongBinaryOperator    long    applyAsLong  (long   left, long   right)
+
+// Predikáty (Predicates) - sloužé k získání logické hodnoty odvozené z hodnoty jejich arhumentu
+// Predicate<T>           boolean test(T      value)
+// DoublePredicate        boolean test(double value)
+// IntPredicate           boolean test(int    value)
+// LongPredicate          boolean test(long   value)
+// BiPredicate<T, U>      boolean test(T t ,U u)
+
 public class AUvod {
 }
