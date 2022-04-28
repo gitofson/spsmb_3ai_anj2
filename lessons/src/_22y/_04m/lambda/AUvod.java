60243232777ec121db45782fbf437cdde166d6f7
stemberk <stemberk@spsmb.cz>
2022-04-28 12:08:52 +0200

    lambda

---
+++ lessons/src/_22y/_04m/lambda/AUvod.java
@@ -0,0 +1,10 @@
+package _22y._04m.lambda;
+// Anonymní třídy vyžadují zbytečně mnoho "syntaktické vaty", která znepřehledňuje výsledný kód.
+// Java 8 přišla s konceptem lambda výrazů, které většinu této "vaty" eliminují. Navíc i samotná
+// implementace lambda výrazů, resp. jejich použití často vede na efektivnější kód.
+
+// Lambda výrazy nahrazují anonymní třídy implementující funkční rozhraní, tj. rozhraní
+// deklarující právě jednu abstraktní metodu. Lambda výrazy se pak vydávají za instance
+// tohoto rozhraní a jimi reprezentovaný kód se spustí tak, že se příslušná metoda zavolá.
+public class AUvod {
+}
