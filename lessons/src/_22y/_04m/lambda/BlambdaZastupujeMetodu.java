3735984e2da37750410317b7c4fa0ffc6ef3a816
stemberk <stemberk@spsmb.cz>
2022-05-02 12:07:32 +0200

    lambda vyrazy

---
+++ lessons/src/_22y/_04m/lambda/BlambdaZastupujeMetodu.java
@@ -0,0 +1,27 @@
+package _22y._04m.lambda;
+// V řadě případů těla lambda-výrazů "degenerují" na pouhá zavolání některé
+// metody, např.
+// expr = x -> method(x);
+
+import java.util.function.ToIntBiFunction;
+import java.util.function.ToIntFunction;
+
+// V takových situacích bývá často výhodnější označit lambda-výraz za
+// zástupce dané metody. Používá se k tomu operátor čtyřtečka. Existují
+// 3 podoby:
+// inst::mtd        - kvalifikace názvu metody názvem instance
+// Cls::mtd         - kvalifikace názvu metody názvem třídy
+// Cls::new         - zvláštní forma, pokud konstgruktor zabalíme do lambda
+//                    výrazu, tak již nevystupuje jako konstruktor, ale
+//                    jako nějaká tovární metoda, která je nám schopna dodat
+//                    na požádání nějaký objekt.
+
+public class BlambdaZastupujeMetodu {
+    public static void main(String[] args) {
+        ToIntFunction<String> cmpI = "Prvni"::compareTo;
+        int inst = cmpI.applyAsInt("Druhý");
+        ToIntBiFunction<String, String> cmpC = String::compareTo;
+        int cls = cmpC.applyAsInt("První", "Druhý");
+        System.out.format("inst: %d, cls: %d%n", inst, cls);
+    }
+}
