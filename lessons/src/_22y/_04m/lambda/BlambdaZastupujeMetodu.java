34ac219a41162866b38891f602d926f22a419143
stemberk <stemberk@spsmb.cz>
2022-05-05 11:15:04 +0200

    BClassTemplate

--- lessons/src/_22y/_04m/lambda/BlambdaZastupujeMetodu.java
+++ lessons/src/_22y/_04m/lambda/BlambdaZastupujeMetodu.java
@@ -3,6 +3,10 @@ package _22y._04m.lambda;
 // metody, např.
 // expr = x -> method(x);
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.GregorianCalendar;
+import java.util.function.Supplier;
 import java.util.function.ToIntBiFunction;
 import java.util.function.ToIntFunction;
 
@@ -20,8 +24,18 @@ public class BlambdaZastupujeMetodu {
     public static void main(String[] args) {
         ToIntFunction<String> cmpI = "Prvni"::compareTo;
         int inst = cmpI.applyAsInt("Druhý");
+
         ToIntBiFunction<String, String> cmpC = String::compareTo;
         int cls = cmpC.applyAsInt("První", "Druhý");
+
         System.out.format("inst: %d, cls: %d%n", inst, cls);
+
+        GregorianCalendar gc = new GregorianCalendar();
+        System.out.println(new SimpleDateFormat("dd.MM.YYYY HH:mm:ss").format(gc.getTime()));
+
+        //Tovární metoda gc_factory.get() pro výrobu uinstance třídy GregorianCalendar
+        Supplier<GregorianCalendar> gc_factory = GregorianCalendar::new;
+        System.out.println(new SimpleDateFormat("dd.MM.YYYY HH:mm:ss").format(gc_factory.get().getTime()));
+
     }
 }
